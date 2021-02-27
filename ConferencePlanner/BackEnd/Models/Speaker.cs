023fb3f813eece3fe05ee64ccfe0182786375657
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-02-27 22:29:08 +0300

    Refactoring the Speaker model into the ConferenceDTO project.

--- ConferencePlanner/BackEnd/Models/Speaker.cs
+++ ConferencePlanner/BackEnd/Models/Speaker.cs
@@ -2,18 +2,7 @@
 
 namespace BackEnd.Models
 {
-    public class Speaker
+    public class Speaker : ConferenceDTO.Speaker
     {
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; }
-
-        [StringLength(4000)]
-        public string Bio { get; set; }
-
-        [StringLength(1000)]
-        public virtual string WebSite { get; set; }
     }
 }
