2885120763552339baba18fc8cac932728dab50f
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-02-27 23:37:22 +0300

    Updating the Speakers API controller.

---
+++ ConferencePlanner/BackEnd/Infrastructure/EntityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BackEnd.Data;
+using System.Linq;
+
+namespace BackEnd.Infrastructure
+{
+    public static class EntityExtensions
+    {
+        public static ConferenceDTO.SpeakerResponse MapSpeakerResponse(this Speaker speaker) =>
+            new ConferenceDTO.SpeakerResponse
+            {
+                Id = speaker.Id,
+                Name = speaker.Name,
+                Bio = speaker.Bio,
+                WebSite = speaker.WebSite,
+                Sessions = speaker.SessionSpeakers?
+                    .Select(ss =>
+                        new ConferenceDTO.Session
+                        {
+                            Id = ss.SessionId,
+                            Title = ss.Session.Title
+                        })
+                    .ToList()
+            };
+    }
+}
