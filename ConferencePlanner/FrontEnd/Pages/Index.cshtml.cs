d3cbf7915c6a722d8145959dde6e97055ca5121e
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-02-28 00:54:08 +0300

    Load the data into the PageModel.

--- ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
+++ ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConferenceDTO;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,16 +12,40 @@ namespace FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>> Sessions { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<(int Offset, DayOfWeek? DayofWeek)> DayOffsets { get; set; }
+
+        public int CurrentDayOffset { get; set; }
+
+        protected readonly IApiClient _apiClient;
+
+        public IndexModel(IApiClient apiClient)
         {
-            _logger = logger;
+            _apiClient = apiClient;
         }
 
-        public void OnGet()
+        public async Task OnGet(int day = 0)
         {
+            CurrentDayOffset = day;
 
+            var sessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+
+            DayOffsets = sessions.Select(s => s.StartTime?.Date)
+                                 .Distinct()
+                                 .OrderBy(d => d)
+                                 .Select(day => ((int)Math.Floor((day.Value - startDate)?.TotalDays ?? 0),
+                                                 day?.DayOfWeek))
+                                 .ToList();
+
+            var filterDate = startDate?.AddDays(day);
+
+            Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+                               .OrderBy(s => s.TrackId)
+                               .GroupBy(s => s.StartTime)
+                               .OrderBy(g => g.Key);
         }
     }
 }
